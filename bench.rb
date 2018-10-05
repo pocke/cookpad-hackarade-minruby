b3ab979db5efc291ada1cbc91b011c3be78a2687
Masataka Pocke Kuwabara <pocke@cookpad.com>
2018-10-05 17:20:28 +0900

    Update bench

--- bench.rb
+++ bench.rb
@@ -1,28 +1,21 @@
 require 'benchmark'
 
+files = Dir.glob('test*.rb')
+
 Benchmark.bm(20) do |x|
   x.report('one') do
-    files = Dir.glob('test*.rb')
     files.each do |f|
       system("ruby interp.rb #{f} > /dev/null", exception: true)
     end
   end
   x.report('two') do
-    files = Dir.glob('test*.rb')
     files.each do |f|
       system("ruby interp.rb interp.rb #{f} > /dev/null", exception: true)
     end
   end
   x.report('three') do
-    files = Dir.glob('test*.rb')
     files.each do |f|
       system("ruby interp.rb interp.rb interp.rb #{f} > /dev/null", exception: true)
-    end
-  end
-  x.report('four') do
-    files = Dir.glob('test*.rb')
-    files.each do |f|
-      system("ruby interp.rb interp.rb interp.rb interp.rb #{f} > /dev/null", exception: true)
     end
   end
 end
