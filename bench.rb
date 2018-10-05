3911a366ed6bcd43e95cc9db5052ac09a511cf22
Masataka Pocke Kuwabara <pocke@cookpad.com>
2018-10-05 18:22:26 +0900

    Drop one and two

--- bench.rb
+++ bench.rb
@@ -3,16 +3,6 @@ require 'benchmark'
 files = Dir.glob('test*.rb')
 
 Benchmark.bm(20) do |x|
-  x.report('one') do
-    files.each do |f|
-      system("ruby interp.rb #{f} > /dev/null", exception: true)
-    end
-  end
-  x.report('two') do
-    files.each do |f|
-      system("ruby interp.rb interp.rb #{f} > /dev/null", exception: true)
-    end
-  end
   x.report('three') do
     files.each do |f|
       system("ruby interp.rb interp.rb interp.rb #{f} > /dev/null", exception: true)
